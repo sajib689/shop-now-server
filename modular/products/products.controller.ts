3ad2f84adb5d81a1a5e072ba4720702ebb0780bb
sajib689 <jibonraja11@gmail.com>
2025-03-20 21:30:24 +0600

    Implement product creation in products controller with success response

--- modular/products/products.controller.ts
+++ modular/products/products.controller.ts
@@ -1,8 +1,15 @@
 import { Request, Response } from "express";
+import { productsService } from "./products.service";
 
 export const productsController = async (req: Request, res: Response) => {
     try {
-
+        const products = req.body;
+        const result = await productsService(products);
+        res.status(200).json({
+            message: "Successfully created product",
+            status: "success",
+            data: result
+        })
     } catch (err) {
         throw new Error("Error")
     }
