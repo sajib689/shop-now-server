485fc0cffdd8cf285c8dd7da808685606043693d
sajib689 <jibonraja11@gmail.com>
2025-03-20 18:12:20 +0600

    add the router for post users data

--- modular/users/users.route.ts
+++ modular/users/users.route.ts
@@ -4,5 +4,6 @@ import { usersController } from "./users.controller"
 const usersRouter = express.Router()
 
 usersRouter.post('/users', usersController)
+// http://localhost:5000/api/v1/users
 
 export default usersRouter;
