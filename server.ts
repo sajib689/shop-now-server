f3676aeb8764b0e3326fddd7c03470a10d6422c8
sajib689 <jibonraja11@gmail.com>
2025-03-20 12:18:43 +0600

    Enhance CORS configuration and rename database connection function

--- server.ts
+++ server.ts
@@ -2,14 +2,20 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-
+import { createServer } from "http";
 const app = express();
 dotenv.config();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+  })
+);
 app.use(express.urlencoded({ extended: true }));
+const server = createServer(app);
 // Connect to MongoDB
-const connectDb = async () => {
+const connectionToDb = async () => {
   try {
     const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.2m0rny5.mongodb.net/learnCode`;
     await mongoose.connect(uri);
@@ -18,3 +24,5 @@ const connectDb = async () => {
     console.log(err);
   }
 };
+
+connectionToDb();
