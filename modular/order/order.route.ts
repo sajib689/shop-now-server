bd1b80ab17e4c81509771aa3cacfa86d6dfc72ef
sajib689 <jibonraja11@gmail.com>
2025-03-22 17:47:46 +0600

    Add orderHistoryController to retrieve order history

--- modular/order/order.route.ts
+++ modular/order/order.route.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { asyncHandler } from './../../middlewares/asyncHandler';
-import { placeOrderController } from "./order.controller";
+import { placeOrderController,orderHistoryController } from "./order.controller";
 
 const orderRouter = express.Router();
 
 // create order router
 orderRouter.post('/order', asyncHandler(placeOrderController))
+orderRouter.get('/orderhistory', asyncHandler(orderHistoryController))
 
 export default orderRouter;
