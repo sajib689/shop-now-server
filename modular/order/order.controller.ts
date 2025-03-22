88ecdc42bc20bc746b481a492a4006c845d2fb2b
sajib689 <jibonraja11@gmail.com>
2025-03-22 17:30:42 +0600

    Add orderHistoryService to retrieve all orders history

--- modular/order/order.controller.ts
+++ modular/order/order.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { placeOrderService } from "./order.service";
+import { orderHistoryService, placeOrderService } from "./order.service";
 
 export const placeOrderController = async (req: Request, res: Response) => {
     try {
@@ -14,3 +14,17 @@ export const placeOrderController = async (req: Request, res: Response) => {
         res.status(400).json({ message: error.message });
     }
 };
+
+// get all orders history
+export const orderHistoryController = async (req: Request, res: Response) => {
+    try {
+        const orders = await orderHistoryService();
+        res.status(200).json({
+            message: "Order history fetched successfully",
+            status: 200,
+            data: orders,
+        });
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+}
