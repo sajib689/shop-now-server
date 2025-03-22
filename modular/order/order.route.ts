d9b46e5fede68a6ecc0ac05ab4b6e574893527d2
sajib689 <jibonraja11@gmail.com>
2025-03-22 17:26:42 +0600

    Add order router to handle order creation requests

---
+++ modular/order/order.route.ts
@@ -0,0 +1,8 @@
+import express from "express";
+import { asyncHandler } from './../../middlewares/asyncHandler';
+import { placeOrderController } from "./order.controller";
+
+const orderRouter = express.Router();
+
+// create order router
+orderRouter.post('/order', asyncHandler(placeOrderController))
